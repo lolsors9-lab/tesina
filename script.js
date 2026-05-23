f15865c98363b12f4bdecc0cc59b5702469c847f
lolsors9-lab <lolsors9@gmail.com>
2026-05-23 17:15:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+// LOADER
+
+window.addEventListener("load", () => {
+
+  setTimeout(() => {
+
+    document.getElementById("loader").style.opacity = "0";
+
+    setTimeout(() => {
+      document.getElementById("loader").style.display = "none";
+    }, 1000);
+
+  }, 3000);
+
+});
+
+// AOS
+
+AOS.init({
+  duration: 1200,
+});
+
+// PARTICLES
+
+tsParticles.load("tsparticles", {
+
+  background: {
+    color: "#050816"
+  },
+
+  particles: {
+
+    number: {
+      value: 100
+    },
+
+    color: {
+      value: ["#00ffff", "#ff00ff"]
+    },
+
+    links: {
+      enable: true,
+      color: "#00ffff",
+      distance: 150,
+      opacity: 0.3
+    },
+
+    move: {
+      enable: true,
+      speed: 2
+    },
+
+    size: {
+      value: 2
+    },
+
+    opacity: {
+      value: 0.5
+    }
+
+  }
+
+});
+
+// 3D EFFECT
+
+VanillaTilt.init(document.querySelectorAll(".card"), {
+
+  max: 15,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.5,
+
+});
+
+// GSAP HERO
+
+gsap.from(".main-title", {
+
+  y: -100,
+  opacity: 0,
+  duration: 1.5,
+
+});
+
+gsap.from(".hero p", {
+
+  opacity: 0,
+  duration: 2,
+  delay: 0.5,
+
+});
+
+gsap.from(".btn", {
+
+  opacity: 0,
+  duration: 2,
+  delay: 1,
+
+});
+
+// CURSOR GLOW
+
+const glow = document.querySelector(".cursor-glow");
+
+window.addEventListener("mousemove", (e) => {
+
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
+
+});
